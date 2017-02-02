747e855bef734b7fb966f55a20370ff37d58a302
nasis <nir@logz.io>
2017-02-02 15:23:40 +0200

    logzioLogger to be created once per type and not per message...

--- index.js
+++ index.js
@@ -12,137 +12,140 @@ var os = require('os');
 var logzioLogger = require('logzio-nodejs');
 
 
+var loggers = {};
+function getOrCreateLogger(type, opts) {
+    var logger = loggers[type];
+    if (!logger) {
+        logger = logzioLogger.createLogger({
+            token: opts.token,
+            protocol: 'https',
+            type: type,
+            bufferSize: 1000,
+            host: opts.zone === 'eu' ? 'listener-eu.logz.io' : '' // US is the default value
+        });
+        loggers[type] = logger;
+    }
+    return logger;
+}
+
 function start(opts) {
-  var type;
-  var noRestart = function() {};
 
+    var filter = through.obj(function (obj, enc, cb) {
+        addAll(opts.add, obj);
 
+        var type = 'docker-unknown';
+        if (obj.line) {
+            type = 'docker-logs';
+        }
+        else if (obj.type) {
+            obj.action_type = obj.type;
+            type = 'docker-events';
+        }
+        else if (obj.stats) {
+            type = 'docker-stats';
+        }
+        obj.host = os.hostname();
 
-  var filter = through.obj(function(obj, enc, cb) {
-    addAll(opts.add, obj);
-    var token = opts.token;
-    var zone = opts.zone;
+        getOrCreateLogger(type, opts).log(obj);
 
-    if (obj.line) {
-      type = 'docker-logs';
-    }
-    else if (obj.type) {
-      obj.action_type = obj.type
-      type = 'docker-events';
-
-    }
-    else if (obj.stats) {
-      type = 'docker-stats';
-    }
-    //console.log(JSON.stringify(obj));
-    obj.host = os.hostname();
-
-    var logger = logzioLogger.createLogger({
-      token: token,
-      protocol: 'https',
-      type: type,
-      bufferSize: 1000,
-      host: zone === 'eu' ? 'listener-eu.logz.io' : '' // US is the default value
+        cb()
     });
 
-    logger.log(obj);
+    var events = allContainers(opts);
+    var loghose;
+    var stats;
+    var dockerEvents;
 
-    cb()
-  });
+    opts.events = events;
 
-  var events = allContainers(opts);
-  var loghose;
-  var stats;
-  var dockerEvents;
-
-  opts.events = events;
-
-  if (opts.logs !== false && opts.token) {
-    loghose = logFactory(opts);
-    loghose.pipe(filter);
-  }
-
-  if (opts.stats !== false && opts.token) {
-    stats = statsFactory(opts);
-    stats.pipe(filter);
-  }
-
-  if (opts.dockerEvents !== false && opts.token) {
-    dockerEvents = eventsFactory(opts);
-    dockerEvents.pipe(filter);
-  }
-
-  if (!stats && !loghose && !dockerEvents) {
-    throw new Error('Please enable one logging facility out of stats, logs or dockerEvents');
-  }
-
-
-  return loghose;
-
-  function addAll(proto, obj) {
-    if (!proto) { return; }
-
-    var key;
-    for (key in proto) {
-      if (proto.hasOwnProperty(key)) {
-        obj[key] = proto[key];
-      }
+    if (opts.logs !== false && opts.token) {
+        loghose = logFactory(opts);
+        loghose.pipe(filter);
     }
-  }
+
+    if (opts.stats !== false && opts.token) {
+        stats = statsFactory(opts);
+        stats.pipe(filter);
+    }
+
+    if (opts.dockerEvents !== false && opts.token) {
+        dockerEvents = eventsFactory(opts);
+        dockerEvents.pipe(filter);
+    }
+
+    if (!stats && !loghose && !dockerEvents) {
+        throw new Error('Please enable one logging facility out of stats, logs or dockerEvents');
+    }
+
+
+    return loghose;
+
+    function addAll(proto, obj) {
+        if (!proto) {
+            return;
+        }
+
+        var key;
+        for (key in proto) {
+            if (proto.hasOwnProperty(key)) {
+                obj[key] = proto[key];
+            }
+        }
+    }
 }
 
 function cli() {
-  var argv = minimist(process.argv.slice(2), {
-    boolean: ['json'],
-    string: ['token'],
-    alias: {
-      'token': 't',
-      'newline': 'n',
-      'json': 'j',
-      'statsinterval': 'i',
-      'add': 'a',
-      'zone': 'z'
-    },
-    default: {
-      json: false,
-      newline: true,
-      stats: true,
-      logs: true,
-      dockerEvents: true,
-      statsinterval: 30,
-      add: [ 'host=' + os.hostname() ],
-      token: process.env.LOGZIO_TOKEN,
-      zone: process.env.LOGZIO_ZONE
+    var argv = minimist(process.argv.slice(2), {
+        boolean: ['json'],
+        string: ['token'],
+        alias: {
+            'token': 't',
+            'newline': 'n',
+            'json': 'j',
+            'statsinterval': 'i',
+            'add': 'a',
+            'zone': 'z'
+        },
+        default: {
+            json: false,
+            newline: true,
+            stats: true,
+            logs: true,
+            dockerEvents: true,
+            statsinterval: 30,
+            add: ['host=' + os.hostname()],
+            token: process.env.LOGZIO_TOKEN,
+            zone: process.env.LOGZIO_ZONE
+        }
+    });
+
+    if (argv.help || !(argv.token)) {
+        console.log('Usage: docker-logzio [-t TOKEN][-j] [--no-newline]\n' +
+            '                         [--no-stats] [--no-logs] [--no-dockerEvents]\n' +
+            '                         [-i STATSINTERVAL] [-a KEY=VALUE] [-z us|eu]\n' +
+            '                         [--matchByImage REGEXP] [--matchByName REGEXP]\n' +
+            '                         [--skipByImage REGEXP] [--skipByName REGEXP]\n' +
+            '                         [--help]');
+
+        process.exit(1);
     }
-  });
-
-  if (argv.help || !(argv.token)) {
-    console.log('Usage: docker-logzio [-t TOKEN][-j] [--no-newline]\n' +
-                '                         [--no-stats] [--no-logs] [--no-dockerEvents]\n' +
-                '                         [-i STATSINTERVAL] [-a KEY=VALUE] [-z us|eu]\n' +
-                '                         [--matchByImage REGEXP] [--matchByName REGEXP]\n' +
-                '                         [--skipByImage REGEXP] [--skipByName REGEXP]\n' +
-                '                         [--help]');
-
-    process.exit(1);
-  }
 
 
-  if (argv.add && !Array.isArray(argv.add)) {
-    argv.add = [argv.add];
-  }
+    if (argv.add && !Array.isArray(argv.add)) {
+        argv.add = [argv.add];
+    }
 
-  argv.add = argv.add.reduce(function(acc, arg) {
-    arg = arg.split('=');
-    acc[arg[0]] = arg[1];
-    return acc
-  }, {});
+    argv.add = argv.add.reduce(function (acc, arg) {
+        arg = arg.split('=');
+        acc[arg[0]] = arg[1];
+        return acc
+    }, {});
 
-  start(argv);
+    start(argv);
 }
 
 module.exports = start;
 
 if (require.main === module) {
-  cli();
+    cli();
 }
