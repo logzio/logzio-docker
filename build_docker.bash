305ac998eb9abede3f391580365fdc9c51078d59
nasis <nir@logz.io>
2017-02-02 18:31:21 +0200

    docker build and push script

---
+++ build_docker.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ `basename $PWD` != "logzio-docker" ]; then
+    echo "This script should be run from logzio-docker directory as it relies on relative paths"
+    exit 1
+fi
+
+source VERSION
+
+DOCKER_REGISTRY=registry.internal.logz.io:5000
+IMAGE_NAME=logzio/logzio-docker
+
+docker build -t $IMAGE_NAME .
+docker tag $IMAGE_NAME $DOCKER_REGISTRY/$IMAGE_NAME:$VERSION
+docker tag $IMAGE_NAME $DOCKER_REGISTRY/$IMAGE_NAME:latest
+
+docker push $DOCKER_REGISTRY/$IMAGE_NAME:$VERSION
+docker push $DOCKER_REGISTRY/$IMAGE_NAME:latest
